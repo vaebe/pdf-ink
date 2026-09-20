c8e02baab7a767fe9b319052cf8fb6093a70179c
zcj <zhangchengjin@gasolinegames.com>
2026-09-20 12:00:50 +0800

    chore: 初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import vue from "@vitejs/plugin-vue";
+import { defineConfig, lazyPlugins } from "vite-plus";
+
+// https://vite.dev/config/
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+  plugins: lazyPlugins(() => [vue()]),
+});
